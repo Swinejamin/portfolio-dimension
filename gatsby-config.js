33a8572b43cf19eaaf20cf15de4817c66bda9a41
swinejamin <bmswine@gmail.com>
2019-02-05 14:18:40 -0600

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Benjamin Swineford | Front End Developer',
+    author: 'Benjamin Swineford',
+    description: 'A portfolio for Benjamin Swineford',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
