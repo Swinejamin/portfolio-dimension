a83b7efd29bfc75e383ea56f32d09313a9b3ac0f
swinejamin <bmswine@gmail.com>
2019-02-04 21:50:44 -0600

    plz work this time...

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,7 +18,7 @@ export default class Contact extends Component {
         <form
           name="contact"
           data-netlify="true"
-          method="post"
+          method="POST"
           action="/success"
           data-netlify-honeypot="bot-field"
         >
